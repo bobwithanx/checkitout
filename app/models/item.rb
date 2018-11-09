48b859ecf0aa71288045917ebbb6e6abd2389371
Bob Marquis <bob@sailbear.net>
2018-11-08 20:22:37 -0700

    Fixed Item scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,8 @@ class Item < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  scope :available, -> { joins(:loans).merge(Loan.active)}
+  scope :unavailable, -> { joins(:loans).merge(Loan.active)}
+  scope :available, -> { where.not(id: unavailable) }
 
   ransack_alias :name_or_tag, :brand_or_name_or_description_or_inventory_tag
 
