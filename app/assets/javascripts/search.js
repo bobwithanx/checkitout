1cae3f3fdaf5bb1470300350f291417b527c3d3f
Bob Marquis <bob@sailbear.net>
2018-11-14 09:33:44 -0700

    Fixed search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,11 +1,11 @@
 document.addEventListener("turbolinks:load", function () {
   input = $("#member-autocomplete");
   var options = {
-    getValue: "id_number",
-    // getValue: function (element) {
-    //   return element.name + " (" +
-    //          element.id_number + ")";
-    // },
+    // getValue: "id_number",
+    getValue: function (element) {
+      return element.name + " (" +
+             element.id_number + ")";
+    },
     url: function (phrase) {
       console.log("phrase = " + phrase);
       return "/members/search.json?q=" + phrase;
@@ -23,7 +23,7 @@ document.addEventListener("turbolinks:load", function () {
         var id_number = input.getSelectedItemData().id_number;
 
         console.log("url = " + url);
-        input.val(id_number);
+        input.val("");
 
         Turbolinks.visit(url);
       },
